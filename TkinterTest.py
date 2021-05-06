ddafcb580c56e2f7f763067ca6322a1d9e771f07
JeromeJohnson700 <79881928+JeromeJohnson700@users.noreply.github.com>
2021-05-06 11:02:44 -0600

    Add files via upload

--- TkinterTest.py
+++ TkinterTest.py
@@ -1,14 +1,23 @@
-From tkinter import *
+from tkinter import *
 
 top = Tk()
+playlist = []
 
-def resulta():
+def results():
     result = E1.get()
-    print(result)
-    print(type(result))
+    playlist.append(result)
+    E1.delete(0, END)
+def printList():
+    print(playlist)
+
+def exportList():
+    with open('test.txt, 'w') as f:
+                for item in playlist:
+                f.write("%s\n" % item)
+  
 
 # This is a Label widget
-L1 = Label(top, text="Hello, world!")
+L1 = Label(top, text="Playlist Generator")
 L1.grid(column= 0, row= 1)
 
 # This is an Entry widget
@@ -16,8 +25,10 @@ E1 = Entry(top, bd = 5)
 E1.grid(column= 0, row= 2)
 
 # This is a Button widget
-B1 = Button(text= "     Get Data   ", bg="red", command= print(E1.get())
-B1.grid(column= 0, row= 3)
+B1 = Button(text= " + ", bg="white", "  Get Data   ", bg="red", command= print(E1.get())
+B1.grid(column= 1, row= 2)
 
-
+B2 = Button(text= " print ", "light blue", command= print(B1.get())
+B2. grid(column= 2, row=2)
 top.mainloop()
+ 
