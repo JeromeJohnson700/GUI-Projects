d00746e6bbd2b90927748d40a6dc333e8fa27970
JeromeJohnson700 <79881928+JeromeJohnson700@users.noreply.github.com>
2021-05-21 10:04:30 -0700

    Add files via upload

--- TkinterTest.py
+++ TkinterTest.py
@@ -8,7 +8,7 @@ dieType = 0
 rollTimes = 0
 
 def results():
-    result = E1.get()
+    result = E1.get
     playlist.append(result)
     E1.delete(0, END)
 def printList():
@@ -27,13 +27,13 @@ def mainMenu():
     clearWindow()
     LMain = Label(top, text = "Block 5 GUI Projects")
     LMain.grid(column = 0, row = 1)
-    
+
     B1Main = Button(text= "week 1", bg= "white", command = week1)
     B1Main.grid(column= 0, row = 2)
-    
+
     B2Main = Button(text = "week 2", bg = "white")
     B2Main.grid(column= 0, row = 3)
-    
+
     B3Main = Button(text = "week 3", bg = "white")
     B3Main.grid(column= 0, row = 4)
 def week1():
@@ -65,7 +65,7 @@ def week1():
     Bexit.grid(column = 1, row = 3)
 
 def week2():
-    def rollDice()
+    def rollDice():
         #update variable data
         dieType = E2Week2.get()
         rollTimes = E1Week2.get()
@@ -76,36 +76,50 @@ def week2():
             myRolls.append(random.randint(1, int(dieType)))
 
         #build the results window
-        L4Week2 = Label()
-        L5Week2 = Label()
-        B2Week2 = Button()
+        L4Week2 = Label(top, text= "Here's your rolls!")
+        L4Week2.grid(column= o, row= 1)
+
+        L5Week2 = Label(top, text="{}".format(myRolls))
+        L5Week2.grid(column= 0, row=2)
+
+        B2Week2 = Button(text+"Main Menu", bg= "yellow", command= mainMenu)
+        B2Week2.grid(column= 0, row=3)
 
 
-
-
-    
     clearWindow()
-    B1Week2 = Button(text= "Roll em!", bg="yellow")
+    B1Week2 = Button(text= "Roll em!", bg="yellow", command= rollDice)
     B1Week2.grid(column = 2, row=4)
-                 
+
     L1Week2 = Label(top, text= "Dice Roller App")
     L1Week2.grid(column= 2, row= 1)
-    
+
     L2Week2 = Label(top, textt= "# of Rolls")
     L2Week2.grid(column= 0, row= 2)
-    
+
     L3Week2 = Label(top, textt= "# of Sides")
     L3Week2.grid(column= 3, row= 2)
-    
-    E1Week2 = Entry()
+
+    E1Week2 = Entry(top, bd = 5)
     E1Week2.grid(column= 0, row= 3)
 
-    E2Week2 = Entry()
+    E2Week2 = Entry(top, bd = 5)
     E2Week2.grid(column= 3, row = 3)
 
 
+def week3():
+    def Block4code():
+        print(text= "Hi")
 
-
+    if len(unique_list) == 0:
+        print(myList)
+    else:
+        wichOne = input("Which list? sorted or un-sorted?  ")
+        if wichOne.lower() == "sorted":
+            print(unique_list)
+        else:
+            print(myList)
+        
+        
 
 
 if __name__== "__main__":
